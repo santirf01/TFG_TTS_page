71becdb354897d85aee26a208cc47b16d1fad35f
santirf01 <121118177+santirf01@users.noreply.github.com>
2023-07-27 13:14:05 +0200

    Create guardar_encuesta.php

---
+++ Encuesta/guardar_encuesta.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "nombre_del_servidor";
+$username = "nombre_de_usuario";
+$password = "contraseña";
+$dbname = "nombre_de_la_base_de_datos";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $nombre = $_POST['nombre'];
+    $edad = $_POST['edad'];
+    // Obtén más respuestas de la encuesta aquí y asigna a variables.
+
+    $sql = "INSERT INTO encuestas (nombre, edad) VALUES (:nombre, :edad)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':nombre', $nombre);
+    $stmt->bindParam(':edad', $edad);
+    // Vincula más variables y columnas de la tabla según las respuestas de la encuesta.
+
+    $stmt->execute();
+
+    echo "Encuesta guardada exitosamente.";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+$conn = null;
+?>
